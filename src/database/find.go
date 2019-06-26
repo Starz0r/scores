7e5c8d87652f371c73eb44be7af9694d6532d5d8
Starz0r <starz0r@starz0r.com>
2019-06-25 20:11:56 -0500

    database: retrieve scores ordinally

--- src/database/find.go
+++ src/database/find.go
@@ -23,3 +23,31 @@ func SelectID(id uint64) (*Score, error) {
 		return &s, nil
 	}
 }
+
+func SelectOrdinally(track, board uint64, limit int, offset int) ([]*Score, error) {
+	if limit > 100 {
+		// TODO: return an actual error
+		return nil, nil
+	}
+	var ss []*Score
+	scores := db.Collection("scores")
+	rs := scores.Find().
+		Where("track_id = ", track).
+		And("board_id = ", board).
+		OrderBy("performance_rating DESC").
+		Limit(limit).
+		Offset(offset)
+
+	err := rs.All(&ss)
+	if err != nil && err != sql.ErrNoRows {
+		logger.Error().
+			Err(err).
+			Msg("Bad parameters or database error.")
+	}
+
+	if err == sql.ErrNoRows {
+		return nil, err
+	} else {
+		return ss, nil
+	}
+}
