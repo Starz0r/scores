19fb9eaf857ecbfa18e511584f85752a6109205a
Starz0r <starz0r@starz0r.com>
2020-08-13 04:52:53 -0500

    algorithms: accuracy didn't get casted to a float64 properly

--- src/algorithms/accuracy.go
+++ src/algorithms/accuracy.go
@@ -3,8 +3,11 @@ package algorithms
 func CalculateAccuracy(crits, nears, errors, maxNotes uint32) float64 {
 	// HACK: maxNotes is 0 until we can confidently determine it's number
 	maxNotes = (crits + nears + errors)
+	if maxNotes == 0 {
+		maxNotes = 1 // HACK: maxNotes also must be non-zero in the case that everything else is also 0
+	}
 
-	critAmt := float64(100 / maxNotes)
+	critAmt := float64(float64(100) / float64(maxNotes))
 	nearAmt := float64(critAmt / 2)
 	return (critAmt * float64(crits)) + (nearAmt * float64(nears))
 }
