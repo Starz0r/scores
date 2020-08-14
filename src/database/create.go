4b5fea954e11d22ddfd2fac9c04be126e1402099
Starz0r <starz0r@starz0r.com>
2020-08-14 02:43:18 -0500

    database: improve semantics on score submissions
    Where a user has previously submitted a score to a board, we will check the performance rating. If the performance rating is higher than last time, then it will update the score instead of adding a new entry. If it is worse, we will discard the score, rewards should still be calculated, but that isn't handled here, so we will return a special error to signal to perform such tasks.

--- src/database/create.go
+++ src/database/create.go
@@ -1,16 +1,62 @@
 package database
 
 import (
+	"database/sql"
+	"errors"
 	"time"
 
 	"github.com/spidernest-go/logger"
 )
 
-func (b *Score) New() error {
-	b.DateCreated = time.Now()
-	// TODO: Make sure something doesn't already exist in the spot [id, track_id, board_id, profile_id]
-	_, err := db.InsertInto("scores").
-		Values(b).
+var ErrUnderperformed = errors.New("score performed worst than previously submitted")
+
+func (s *Score) New() error {
+	// check for a previous score by the same profile
+	scores := db.Collection("scores")
+	prevscore := *new(Score)
+	err := scores.Find().
+		Where("track_id = ", s.TrackID).
+		And("board_id = ", s.BoardID).
+		And("profile_id = ", s.ProfileID).
+		One(&prevscore)
+
+		// update the score if the performance rating is higher
+	if err == sql.ErrNoRows {
+		if s.PerformanceRating > prevscore.PerformanceRating {
+			s.ID = prevscore.ID
+			stmt, err := db.Update("scores").
+				Set(s).
+				Prepare()
+			if err != nil {
+				logger.Error().
+					Err(err).
+					Msg("SQL Statement on score update couldn't be prepared.")
+
+				return err
+			}
+
+			_, err = stmt.Exec()
+			if err != nil {
+				logger.Error().
+					Err(err).
+					Msg("Score entry could not be updated.")
+			}
+
+			return err
+		} else {
+			return ErrUnderperformed
+		}
+	} else if err != nil && err != sql.ErrNoRows {
+		logger.Error().
+			Err(err).
+			Msg("Bad parameters or database error.")
+
+		return err
+	}
+
+	s.DateCreated = time.Now()
+	_, err = db.InsertInto("scores").
+		Values(s).
 		Exec()
 
 	if err != nil {
